24bbf4ec195bbd61988a7024f023c9d80144e77b
TongguangZhang <tonggz250@gmail.com>
2023-08-20 11:48:08 -0400

    intro to modules

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http');
+var dt = require('./date');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.write('The Date and Time is:' + dt.dateTime());
+  res.end();
 }).listen(8080);
