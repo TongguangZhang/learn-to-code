ea445e17c8445078638abdfb91271a55e58222b1
TongguangZhang <tonggz250@gmail.com>
2023-08-20 12:11:55 -0400

    HTTPS module

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var http = require('http');
-var dt = require('./date');
+var url = require('url');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write('The Date and Time is:' + dt.dateTime());
-  res.end();
+  var q = url.parse(req.url, true).query;
+  var txt = q.year + " " + q.month;
+  res.end(txt);
 }).listen(8080);
