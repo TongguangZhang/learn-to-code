6169a2226eb6e204a17b43f3fc8cf87390a0638f
TongguangZhang <tonggz250@gmail.com>
2023-08-21 17:49:30 -0400

    connecting to database and config file

---
+++ nodejs/database/server.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql2');
+var fs = require('fs');
+
+var configData = fs.readFileSync('config.json');
+var config = JSON.parse(configData);
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: config.username,
+    password: config.password
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected");
+});
