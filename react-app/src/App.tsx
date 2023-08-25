57f1af101420f3b9c4f419a88476e7408c62725e
TongguangZhang <tonggz250@gmail.com>
2023-08-24 23:35:29 -0400

    react button

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,20 @@
 // import ListGroup from "./components/ListGroup";
+import { useState } from "react";
 import Alert from "./components/Alert";
+import Button from "./components/Button";
 
 function App() {
     // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // Hook
     // const handleSelectItem = (item: string) => console.log(item);
+    const [buttonState, setAlertState] = useState(false);
 
     return (
         <div>
             {/* <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup> */}
-            <Alert>Hello World</Alert>
+            {buttonState && <Alert onClick={() => setAlertState(false)}>Alert</Alert>}
+            <Button color="danger" onClick={() => setAlertState(true)}>
+                Button
+            </Button>
         </div>
     );
 }
--- react-app/src/components/Alert.tsx
+++ react-app/src/components/Alert.tsx
@@ -1,11 +1,22 @@
-import { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 
 interface Props {
     children: ReactNode;
+    onClick: () => void;
 }
 
-const Alert = ({ children }: Props) => {
-    return <div className="alert alert-primary">{children}</div>;
+const Alert = ({ children, onClick }: Props) => {
+    return (
+        <div className="alert alert-warning alert-dismissible">
+            {children}
+            <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="alert"
+                onClick={onClick}
+            ></button>
+        </div>
+    );
 };
 
 export default Alert;
