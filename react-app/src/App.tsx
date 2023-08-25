a26c4d9ed02fe9b432543ac3c102017e0888f55d
TongguangZhang <tonggz250@gmail.com>
2023-08-24 22:48:08 -0400

    react passing children

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,12 +1,14 @@
-import ListGroup from "./components/ListGroup";
+// import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // Hook
-    const handleSelectItem = (item: string) => console.log(item);
+    // let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // Hook
+    // const handleSelectItem = (item: string) => console.log(item);
 
     return (
         <div>
-            <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup>
+            {/* <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup> */}
+            <Alert>Hello World</Alert>
         </div>
     );
 }
