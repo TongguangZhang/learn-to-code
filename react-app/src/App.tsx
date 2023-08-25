09b22a23895980f8cf2c68508c6c65081969bb1e
TongguangZhang <tonggz250@gmail.com>
2023-08-24 22:40:30 -0400

    react state and props

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,9 +1,12 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
+    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // Hook
+    const handleSelectItem = (item: string) => console.log(item);
+
     return (
         <div>
-            <ListGroup></ListGroup>
+            <ListGroup items={items} heading={"Cities"} onSelectItem={handleSelectItem}></ListGroup>
         </div>
     );
 }
--- react-app/src/components/ListGroup.tsx
+++ react-app/src/components/ListGroup.tsx
@@ -1,19 +1,32 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
-function ListGroup() {
-    let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+interface Props {
+    items: string[];
+    heading: string;
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+    const [selectedIndex, setSelectedIndex] = useState(-1);
 
     const message = items.length === 0 && <p>No item found</p>;
 
-    const handleClick = (event: MouseEvent) => console.log(event);
-
     return (
         <>
-            <h1>List</h1>
+            <h1>{heading}</h1>
             {message}
             <ul className="list-group">
                 {items.map((item, index) => (
-                    <li className="list-group-item" key={item} onClick={handleClick}>
+                    <li
+                        className={
+                            selectedIndex === index ? "list-group-item active" : "list-group-item"
+                        }
+                        key={item}
+                        onClick={() => {
+                            setSelectedIndex(index);
+                            onSelectItem(item);
+                        }}
+                    >
                         {item}
                     </li>
                 ))}
